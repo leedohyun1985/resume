aee5ffc6ac5435952bb5ce95ced9a1c8531d2575
Dohyun Lee(이도현) <leedohyun1985@gmail.com>
2021-06-03 23:31:21 +0900

    20210603 이도현
    방문자 로그 추가

--- src/main/java/com/doh/resume/controller/ResumeController.java
+++ src/main/java/com/doh/resume/controller/ResumeController.java
@@ -2,7 +2,11 @@ package com.doh.resume.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +19,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ResumeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ResumeController.class);
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(HttpServletRequest request, Locale locale, Model model) {
+		logger.info("방문자 발생");
+		logger.info("request.getLocale() : " + request.getLocale());
+		logger.info("request.getRequestURL() : " + request.getRequestURL());
+		logger.info("request.getRemoteAddr() : " + request.getRemoteAddr());
+		logger.info("request.getRemoteHost() : " + request.getRemoteHost());
+		logger.info("request.getRemotePort() : " + request.getRemotePort());
+		logger.info("request.getRemoteUser() : " + request.getRemoteUser());
+		for (Enumeration<String> headerrNames = request.getHeaderNames(); headerrNames.hasMoreElements();) {
+			logger.info(headerrNames.nextElement() + " : " + request.getHeader(headerrNames.nextElement()));
+		}
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -31,5 +44,5 @@ public class ResumeController {
 
 		return "resume";
 	}
-	
+
 }
